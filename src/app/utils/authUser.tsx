1de79e9b27c5f2e9a0af314a4318249b08b8deb8
Capio21 <130652957+Capio21@users.noreply.github.com>
2025-03-14 14:53:28 +0800

    Update authUser.tsx

--- src/app/utils/authUser.tsx
+++ src/app/utils/authUser.tsx
@@ -1,40 +1,40 @@
-'use client';
+// 'use client';
 
-import { useEffect, useState } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
-import { toast, ToastContainer } from 'react-toastify';
+// import { useEffect, useState } from 'react';
+// import { useRouter, usePathname } from 'next/navigation';
+// import { toast, ToastContainer } from 'react-toastify';
 
-const authUser = (WrappedComponent: React.FC) => {
-    return function AuthenticatedComponent(props: any) {
-        const [isAuthorized, setIsAuthorized] = useState(false);
-        const router = useRouter();
-        const pathname = usePathname();
+// const authUser = (WrappedComponent: React.FC) => {
+//     return function AuthenticatedComponent(props: any) {
+//         const [isAuthorized, setIsAuthorized] = useState(false);
+//         const router = useRouter();
+//         const pathname = usePathname();
 
-        console.log(`Pathname: ${pathname}`)
+//         console.log(`Pathname: ${pathname}`)
 
-        useEffect(() => {
-            const token = sessionStorage.getItem('authToken');
+//         useEffect(() => {
+//             const token = sessionStorage.getItem('authToken');
 
-            if (token && pathname == '/') {
-                sessionStorage.clear()
-                router.push('/');
-            }
+//             if (token && pathname == '/') {
+//                 sessionStorage.clear()
+//                 router.push('/');
+//             }
 
-            if (token || pathname == "/") {
-                setIsAuthorized(true); // Allow user to continue using the system
-            } else {
-                router.push('/');
-            }
-        }, [pathname]);
+//             if (token || pathname == "/") {
+//                 setIsAuthorized(true); // Allow user to continue using the system
+//             } else {
+//                 router.push('/');
+//             }
+//         }, [pathname]);
 
 
 
-        if (!isAuthorized) {
-            return null;
-        }
+//         if (!isAuthorized) {
+//             return null;
+//         }
 
-        return <WrappedComponent {...props} />;
-    };
-};
+//         return <WrappedComponent {...props} />;
+//     };
+// };
 
-export default authUser;
+// export default authUser;
